c2dab892098976a824b4034242d8beeb1af8a2e8
Vitor Aloisio <vitor.aloisio@vitortec.com>
2016-11-07 14:11:56 -0300

    Create config.php

---
+++ pagseguro/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$environment = "sandbox"; //sandbox or production
+$email = "";
+$tokenSandbox = ""; //token sandbox
+$tokenProduction = ""; //token production
+
+$currency = "BRL";
+$corporation = "";
+$payment_method_id = "1";
+
+//InvoicePlane
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "";
+
+?>

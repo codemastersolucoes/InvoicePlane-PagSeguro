a1b6f3f758626817efacac07e1abb9312ffeb284
Vitor Aloisio <vitor.aloisio@vitortec.com>
2017-04-06 16:08:41 -0300

    Create config.php

---
+++ invoiceplane/pagseguro/config.php
@@ -0,0 +1,19 @@
+<?php
+
+define("APP_ENVIRONMENT", "sandbox"); //sandbox or production
+define("APP_MAIL", ""); //PagSeguro user
+define("APP_TOKEN_SANDBOX", ""); //token sandbox
+define("APP_TOKEN_PRODUCTION", ""); //token production
+
+
+define("APP_CURRENCY", "BRL"); // true or false
+define("APP_CORPORATION", "");
+define("APP_PAYMENT_METHOD_ID", "1");
+
+//Data Base
+define("APP_DB_LOCAL", "localhost");
+define("APP_DB_USER", "");
+define("APP_DB_NAME", "");
+define("APP_DB_PASS", "");
+
+?>
